5979dd4c6e1e207c04285f5d9647f1c550c9c0ab
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-14 01:19:47 -0300

    feat: add loginForm to LoginPage

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,18 +1,17 @@
 import wallpaper from '../../../assets/images/backgrounds/wallpaperColor.svg';
-import { Button } from '../../../components/ui/Button';
+import { LoginForm } from '../components/LoginForm';
 
 export function LoginPage() {
   return (
     <main 
-        className="min-h-screen bg-cover bg-center bg-no-repeat"
+        className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center px-6 md:px-64"
       style={{
         backgroundImage: `url(${wallpaper})`,
       }}
     >
-      <h1>Login Page</h1>
-      <Button>Login</Button>
-      -----------
-      <Button variant="secondary">Login</Button>
+      <div className="w-full max-w-md">
+        <LoginForm/>
+      </div>
     </main>
   );
 }
