daff9151bc5bf5289c57f1925af8a12597e1ea4e
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-21 00:17:02 -0300

    feat: add components to page

--- src/features/auth/pages/RegisterPage.tsx
+++ src/features/auth/pages/RegisterPage.tsx
@@ -1,14 +1,28 @@
-import wallpaper from '../../../assets/images/backgrounds/wallpaper.svg';
+import wallpaperDesktop from '../../../assets/images/backgrounds/wallpaperColor.svg';
+import wallpaperMobile from '../../../assets/images/backgrounds/wallpaper.svg';
+import { RegisterForm } from '../components/RegisterForm';
+import { Text } from '../../../components/ui/Text';
+import { Button } from '../../../components/ui/Button';
+import { useNavigate } from 'react-router-dom';
 
 export function RegisterPage() {
+  const navigate = useNavigate();
+
   return (
     <main 
-        className="min-h-screen bg-cover bg-center bg-no-repeat"
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center px-6 md:px-32 bg-[image:var(--bg-mobile)] md:bg-[image:var(--bg-desktop)]"
       style={{
-        backgroundImage: `url(${wallpaper})`,
+        '--bg-mobile': `url(${wallpaperMobile})`,
+        '--bg-desktop': `url(${wallpaperDesktop})`,
       }}
     >
-      <h1>Register Page</h1>
+      <div className="w-full max-w-md flex flex-col">
+        <Text variant="title" className="text-center">IFNITE</Text>
+        <RegisterForm />
+        <Button type="button" variant="ghost" onClick={() => navigate("/login")}>
+          Cancelar
+        </Button>
+      </div>
     </main>
   );
 }
