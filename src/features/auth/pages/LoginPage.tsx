1d6cc1ea1e3449ba323320d3ff7a92426cdd661b
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-21 00:16:21 -0300

    feat: add Criar Conta Button

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -2,8 +2,12 @@ import wallpaperDesktop from '../../../assets/images/backgrounds/wallpaperColor.
 import wallpaperMobile from '../../../assets/images/backgrounds/wallpaper.svg';
 import { LoginForm } from '../components/LoginForm';
 import { Text } from '../../../components/ui/Text';
+import { Button } from '../../../components/ui/Button';
+import { useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
+  const navigate = useNavigate();
+
   return (
     <main 
       className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center px-6 md:px-32 bg-[image:var(--bg-mobile)] md:bg-[image:var(--bg-desktop)]"
@@ -12,9 +16,12 @@ export function LoginPage() {
         '--bg-desktop': `url(${wallpaperDesktop})`,
       }}
     >
-      <div className="w-full max-w-md flex flex-col gap-6">
+      <div className="w-full max-w-md flex flex-col">
         <Text variant="title" className="text-center">IFNITE</Text>
         <LoginForm/>
+        <Button type="button" variant="ghost" onClick={() => navigate("/register")}>
+          Criar conta
+        </Button>
       </div>
     </main>
   );
