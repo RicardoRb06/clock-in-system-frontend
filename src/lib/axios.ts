9b1f29a84e99d77186772d99b5330050c79622e3
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-23 23:18:07 -0300

    feat: console log request

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,16 @@ export const api = axios.create({
     baseURL: env.VITE_API_URL,
     withCredentials: true,
 });
+
+api.interceptors.request.use(
+    (config) => {
+        console.log("HEADER: ", config.headers);
+        console.log("DATA: ", config.data);
+
+        return config;
+    }, 
+    (error) => {
+        console.error();
+        return Promise.reject(error);
+    }
+)
