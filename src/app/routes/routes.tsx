ebce4c8e7260202d3c02961b8ff8e883610f095b
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-01 00:06:28 -0300

    vove to routes/

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import { LoginPage } from "../features/auth/pages/Login.page";
-import { RegisterPage } from "../features/auth/pages/Register.page";
+import { LoginPage } from "../../features/auth/pages/Login.page";
+import { RegisterPage } from "../../features/auth/pages/Register.page";
 
 export function AppRoutes() {
   return (
