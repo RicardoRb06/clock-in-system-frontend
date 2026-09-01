f09d0bcdc07d59d4479c05245ef3d648a402a771
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-31 23:54:05 -0300

    feat: add error message to useAuth

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -5,7 +5,7 @@ export function useAuth() {
     const context = useContext(AuthContext);
 
     if(!context) {
-        throw new Error();
+        throw new Error("useAuth must be used within an AuthProvider");
     }
 
     return context;
