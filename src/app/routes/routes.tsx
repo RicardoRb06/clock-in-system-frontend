13cb085878a1298e85ea662291397ab4bbd59ad8
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-01 00:47:32 -0300

    feat: add protected routes and public routes

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,13 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 
+import { PublicRoutes } from "./public.routes";
+import { ProtectedRoutes } from "./protected.routes";
+
 import { LoginPage } from "../../features/auth/pages/Login.page";
 import { RegisterPage } from "../../features/auth/pages/Register.page";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route element={<PublicRoutes />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" />
+      </Route>
     </Routes>
   );
 }
