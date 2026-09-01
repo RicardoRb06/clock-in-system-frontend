920ba5644b5fa55fc340e02466e74ef2cb9361d4
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-31 23:46:08 -0300

    feat: add refreshUser o update conetext

--- src/features/auth/hooks/useRegister.ts
+++ src/features/auth/hooks/useRegister.ts
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { register } from '../api/authApi.ts'
+import { useAuth } from './useAuth.ts';
 
 export function useRegister() {
+    const { refreshUser } = useAuth();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
@@ -11,6 +13,8 @@ export function useRegister() {
             setError(null);
 
             const response = await register({name, password, category});
+
+            await refreshUser();    
 
             return {success: true, data: response};
         } catch (e){
