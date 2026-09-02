6afa89dfb1873758f5368b3826ca359c35196972
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-02 00:11:48 -0300

    move to pages

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,8 +1,8 @@
 import wallpaperDesktop from '../../../assets/images/backgrounds/wallpaperColor.svg';
 import wallpaperMobile from '../../../assets/images/backgrounds/wallpaper.svg';
-import { LoginForm } from '../components/Login.form';
-import { Text } from '../../../components/ui/Text';
-import { Button } from '../../../components/ui/Button';
+import { LoginForm } from '../features/auth/components/Login.form';
+import { Text } from '../components/ui/Text';
+import { Button } from '../components/ui/Button';
 import { useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
