153528ec15e74715842ba0251d8d80435353672b
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-02 00:14:22 -0300

    fix: update imports

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,5 +1,5 @@
-import wallpaperDesktop from '../../../assets/images/backgrounds/wallpaperColor.svg';
-import wallpaperMobile from '../../../assets/images/backgrounds/wallpaper.svg';
+import wallpaperDesktop from '../assets/backgrounds/wallpaperColor.svg';
+import wallpaperMobile from '../assets/backgrounds/wallpaper.svg';
 import { LoginForm } from '../features/auth/components/Login.form';
 import { Text } from '../components/ui/Text';
 import { Button } from '../components/ui/Button';
