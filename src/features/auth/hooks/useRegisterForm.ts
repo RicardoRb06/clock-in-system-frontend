2d071c3051a1a25297f8e3fc4bf1e6ada7d95602
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-02 15:30:55 -0300

    fix: accept password with 8 characters

--- src/features/auth/hooks/useRegisterForm.ts
+++ src/features/auth/hooks/useRegisterForm.ts
@@ -40,7 +40,7 @@ export function useRegisterForm() {
 
         if(!data.password.trim()){
             newErrors.password = "Senha é obrigatória";
-        } else if(data.password.trim().length <= 8){
+        } else if(data.password.trim().length < 8){
             newErrors.password = "Senha deve ter no mínimo 8 caracteres"
         } else if(data.password.trim().length > 64){
             newErrors.password = "Senha deve ter no máximo 64 caracteres"
