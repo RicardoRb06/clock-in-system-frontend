0bfc2fbf30175f57f5f436c64aacde746bf389e5
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-02 13:56:52 -0300

    fix: add as const

--- src/features/auth/types/roles.ts
+++ src/features/auth/types/roles.ts
@@ -3,6 +3,6 @@ export const rolesOptions = [
     { value: "ADMIN", label: "administrador"},
     { value: "MODERATOR", label: "moderador"},
     { value: "TIME_CLOCK", label: "terminal de ponto"},
-];
+] as const;
 
 export type Role = typeof rolesOptions[number]["value"];
