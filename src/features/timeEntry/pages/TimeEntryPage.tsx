5333a53d97a52b7259c6d05241f2fae113371449
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-07 15:57:43 -0300

    feat: add tailwind class to div

--- src/features/timeEntry/pages/TimeEntryPage.tsx
+++ src/features/timeEntry/pages/TimeEntryPage.tsx
@@ -1,6 +1,6 @@
 export function TimeEntryPage() {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen bg-pageBg">
             <h1>Time Entry Page</h1>
         </div>
     );
