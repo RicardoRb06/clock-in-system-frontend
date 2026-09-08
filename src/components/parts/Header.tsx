1b567426ac73a892547f6ba379e371f4e8485a57
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-07 23:46:54 -0300

    feat: acept children

--- src/components/parts/Header.tsx
+++ src/components/parts/Header.tsx
@@ -1,20 +1,23 @@
 import icon from '@/assets/icons/logoBranca.svg';
 import { Text } from '@/components/ui/Text';
-import { Button } from '../ui/Button';
+import type React from 'react';
 
 interface HeaderProps {
-    userName: string;
-    showLogoutOnly?: boolean;
+    scheme: "scheme-1" | "scheme-2" | "scheme-3" | "scheme-4";
+    children?: React.ReactNode;
 }
 
-export function Header({ userName, showLogoutOnly = false }: HeaderProps) {
+export function Header({ scheme = "scheme-1", children }: HeaderProps) {
     return (
-        <header className="w-full h-18 bg-white border-b border-gray-200 p-4 flex items-center justify-between  shadow-sm">
+        <header className={`${scheme} w-full h-18 bg-scheme-bg p-4 flex items-center justify-between `}>
             <div className="flex items-center gap-3">
-                <img src={icon} alt="Logo" className="h-14 w-auto object-contain" />
-                <Text variant="title" className="text-2xl">
+                <img src={icon} alt="Logo" className="h-12 w-auto object-contain" />
+                <Text scheme={scheme} variant="title" className="text-2xl">
                     IFNITE
                 </Text>
+            </div>
+            <div>
+                {children}
             </div>
         </header>
     );
