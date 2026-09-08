3e9aa2ca6086345e597ae310d27da90527331074
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-07 21:24:54 -0300

    feat: create test page

---
+++ src/pages/testPage.tsx
@@ -0,0 +1,7 @@
+export function TestPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-pageBg">
+            <h1>Test Page</h1>
+        </div>
+    );
+}
