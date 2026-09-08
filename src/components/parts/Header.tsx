abcf34e87a69cf49ae8c696666be89d2da618faf
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-08 00:27:53 -0300

    fix: remove size

--- src/components/parts/Header.tsx
+++ src/components/parts/Header.tsx
@@ -12,7 +12,7 @@ export function Header({ scheme = "scheme-1", children }: HeaderProps) {
         <header className={`${scheme} w-full h-18 bg-scheme-bg p-4 flex items-center justify-between `}>
             <div className="flex items-center gap-3">
                 <img src={icon} alt="Logo" className="h-12 w-auto object-contain" />
-                <Text scheme={scheme} variant="title" className="text-2xl">
+                <Text scheme={scheme} variant="title">
                     IFNITE
                 </Text>
             </div>
