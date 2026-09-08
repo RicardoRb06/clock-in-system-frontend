13710692457804b2f12ad7408b792706e03a3d6d
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-08 11:59:15 -0300

    feat: reposition texts

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -9,10 +9,10 @@ interface BannerProps {
 
 export function Banner ({ accent, title, description, className = ''}: BannerProps){
     return(
-        <div className={`flex flex-col gap-1${className}`}>
-            <Text variant="accent" className="text-[16px] font-semibold">{accent}</Text>
-            <Text variant="title" className="text-[40px]">{title}</Text>
-            <Text variant="body" className="text-l text-neutral-800 mt-1">{description}</Text>
+        <div className={`flex flex-col ${className}`}>
+            <Text variant="accent" className="text-[16px] font-semibold leading-none">{accent}</Text>
+            <Text variant="title" className="text-[40px] leading-tight">{title}</Text>
+            <Text variant="body" className="text-l text-neutral-800 mt-4">{description}</Text>
         </div>
     )
 }
