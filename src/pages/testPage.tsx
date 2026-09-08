e4589bbc0ceac035d6805d40510221bf5cabdf6e
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-08 00:45:02 -0300

    feat: add buttons to test

--- src/pages/testPage.tsx
+++ src/pages/testPage.tsx
@@ -1,7 +1,14 @@
+import { Button } from '../components/ui/Button';
+
 export function TestPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-pageBg">
-            <h1>Test Page</h1>
+            <Button variant="primary">Aperte aqui</Button>
+            <Button variant="primaryAlternative">Aperte aqui</Button>
+            <Button variant="secondary">Aperte aqui</Button>
+            <Button variant="secondaryAlternative">Aperte aqui</Button>
+            <Button variant="link">Aperte aqui</Button>
+            <Button variant="linkAlternative">Aperte aqui</Button>
         </div>
     );
 }
