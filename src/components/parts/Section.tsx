200b377a623ef808b7dd49d2ec6ccabea4a4b8b5
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-08 11:53:35 -0300

    feat: add classnamr

--- src/components/parts/Section.tsx
+++ src/components/parts/Section.tsx
@@ -2,12 +2,13 @@ import type React from "react";
 
 interface SectionProps {
     scheme?: "scheme-1" | "scheme-2" | "scheme-3" | "scheme-4",
+    className?: string,
     children: React.ReactNode;
 }
 
-export function Section ({scheme = "scheme-1", children}: SectionProps) {
+export function Section ({scheme = "scheme-1", className = '', children}: SectionProps) {
     return (
-        <div className={`${scheme} w-full py-10 px-6 md:px-16 lg:px-28`}>
+        <div className={`${scheme} w-full py-10 px-6 md:px-16 lg:px-28 ${className}`}>
             {children}
         </div>
     )
