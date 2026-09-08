320ce3520330f5b819cc5f068e757b8c67996d65
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-08 11:48:27 -0300

    feat: remove sheme

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,16 +1,15 @@
 import { Text } from './Text';
 
 interface BannerProps {
-    scheme: "scheme-1" | "scheme-2" | "scheme-3" | "scheme-4";
     accent?: string;
     title?: string;
     description?: string;
     className?: string;
 }
 
-export function Banner ({scheme = 'scheme-1', accent, title, description, className = ''}: BannerProps){
+export function Banner ({ accent, title, description, className = ''}: BannerProps){
     return(
-        <div className={`${scheme} flex flex-col gap-1${className}`}>
+        <div className={`flex flex-col gap-1${className}`}>
             <Text variant="accent" className="text-[16px] font-semibold">{accent}</Text>
             <Text variant="title" className="text-[40px]">{title}</Text>
             <Text variant="body" className="text-l text-neutral-800 mt-1">{description}</Text>
