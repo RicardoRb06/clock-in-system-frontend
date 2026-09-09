e839c52242a438eea68f869fb52d4e39fd5793b7
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-09 00:49:06 -0300

    feat: add pages onClick to buttons

--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,16 +1,18 @@
 import { Header } from "../parts/header";
 import { Footer } from '../parts/Footer';
 import { Button } from '../ui/Button';
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function UserLayout(){
+    const navigate = useNavigate();
+
     return(
         <div className="min-h-dvh flex flex-col">
             <Header scheme="scheme-2">
-                <Button variant="linkAlternative">Página inicial</Button>
-                <Button variant="linkAlternative">Minhas horas</Button>
-                <Button variant="linkAlternative">Tarefas</Button>
-                <Button variant="linkAlternative">Equipes</Button>
+                <Button variant="linkAlternative" onClick={() => navigate("/homepage")}>Página inicial</Button>
+                <Button variant="linkAlternative" onClick={() => navigate("/hours")}>Minhas horas</Button>
+                <Button variant="linkAlternative" onClick={() => navigate("/tasks")}>Tarefas</Button>
+                <Button variant="linkAlternative" onClick={() => navigate("/Teams")}>Equipes</Button>
             </Header>
             <Outlet />
             <Footer scheme="scheme-2"/>
