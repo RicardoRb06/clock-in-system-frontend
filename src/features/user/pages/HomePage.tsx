8d33b2de9f6982660a6b73ec51b4940e96148092
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-09 00:37:40 -0300

    feat: add navigate to buttons

--- src/features/user/pages/HomePage.tsx
+++ src/features/user/pages/HomePage.tsx
@@ -3,17 +3,19 @@ import { Button } from '../../../components/ui/Button';
 import { useAuth } from '@/features/auth/hooks/useAuth';
 import { Section } from '@/components/parts/Section';
 import { Card } from '@/components/ui/Card';
+import { useNavigate } from 'react-router-dom';
 
 export function HomePage () {
     const { user } = useAuth();
+    const navigate = useNavigate();
 
     return (
         <div className="flex-1 flex flex-col">
             <Section scheme="scheme-1" className="my-8">
                 <Banner accent="Página inicial" title={`Olá, ${user?.name}`} description="Confira suas horas e atividades." />
                 <div className="flex gap-4 mt-4">
-                    <Button>Ver Tarefas</Button>
-                    <Button>Ver Horas</Button>
+                    <Button onClick={() => navigate("/tasks")}>Ver Tarefas</Button>
+                    <Button onClick={() => navigate("/hours")}>Ver Horas</Button>
                 </div>
             </Section>
             <Section scheme="scheme-3" className="flex-1">
