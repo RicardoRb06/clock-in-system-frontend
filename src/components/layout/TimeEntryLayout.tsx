f55af07bce56fab90661461d93ee4b76d99a1b5a
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-09 16:40:19 -0300

    feat: add layout to time entry

--- src/components/layout/TimeEntryLayout.tsx
+++ src/components/layout/TimeEntryLayout.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../parts/header";
+import { Footer } from '../parts/Footer';
+import { Button } from "../ui/Button";
+import { useAuth } from "@/features/auth/hooks/useAuth";
 
 export function TimeEntryLayout() {
+    const { logout } = useAuth()
+
     return (
-        <div>
-            <Header />
+        <div className="min-h-dvh flex flex-col">
+            <Header scheme="scheme-2">
+                <Button variant="linkAlternative" onClick={() => logout()}>Sair</Button>
+            </Header>
             <Outlet />
+            <Footer scheme="scheme-2"/>
         </div>
     );
 }
