0d5e8827be7d49d1cd85e4531596731c6bbaff9f
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-09 21:02:11 -0300

    fix: put _ before vairables

---
+++ src/features/timeEntry/types/tableTimeEntryColumns.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/Button';
+import type { Column } from '@/components/ui/Table';
+import type { AuthUser } from '@/features/auth/types/user';
+
+export const timeEntryColumns: Column<AuthUser>[] = [
+    {
+        key: "name",
+        label: "Nome",
+        render: (user) => user._name,
+    },
+    {
+        key: "category",
+        label: "Equipe",
+        render: (user) => user._category ?? "",
+    },
+    {
+        key: "role",
+        label: "Cargo",
+        render: (user) => user._role,
+    },
+    {
+        key: "hours",
+        label: "Ver Horas",
+        render: () => (<Button>Ver horas</Button>),
+    },
+    {
+        key: "timeEntry",
+        label: "Abrir/Fechar Ponto",
+        render: () => (<Button>Ver horas</Button>),
+    },
+];
